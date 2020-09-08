83e62392b00d03cf213f0d669ece031758de0489
António Dias <adias@mosano.eu>
2020-09-08 18:00:16 +0100

    Update keymap

--- keyboards/crkbd/keymaps/antonioxdias/keymap.c
+++ keyboards/crkbd/keymaps/antonioxdias/keymap.c
@@ -13,17 +13,17 @@ extern uint8_t is_master;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _ADJUST 3
+#define _NumNav 1
+#define _Symbols 2
+#define _FMedia 3
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  ADJUST,
-  BACKLIT,
-  RGBRST
+  NumNav,
+  Symbols,
+  FMedia,
+  SHRUG,
+  KEYMAP,
 };
 
 enum macro_keycodes {
@@ -35,16 +35,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-MT(MOD_LCTL,KC_ESC),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT ,\
+MT(MOD_LCTL,KC_ESC),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_MINS, KC_RSPC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, KC_LALT,LT(3,KC_SPC), KC_SPC,   MO(1),   MO(2) \
+                                          KC_LGUI, KC_LALT,LT(3,KC_SPC), KC_SPC,   MO(2),   MO(1) \
                                       //`--------------------------'  `--------------------------'
 
   ),
 
-  [_LOWER] = LAYOUT( \
+  [_NumNav] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_PSCR,   KC_NO,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_HOME, KC_PGUP, KC_PGDN,  KC_END,   KC_NO,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -56,25 +56,25 @@ MT(MOD_LCTL,KC_ESC),KC_NO, KC_4,    KC_5,    KC_6,    KC_0,                     
                                       //`--------------------------'  `--------------------------'
     ),
 
-  [_RAISE] = LAYOUT( \
+  [_Symbols] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_BSLS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_AMPR, KC_SLSH, KC_LPRN, KC_RPRN, KC_QUOT, KC_QUES,\
+      KC_PIPE, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_AMPR, KC_SLSH, KC_QUOT, KC_DQUO, KC_ASTR, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-MT(MOD_LCTL,KC_ESC),KC_NO,KC_NO,   KC_NO,   KC_NO, KC_LCBR,                      KC_RCBR, KC_CIRC, KC_TILD,  KC_GRV, KC_DQUO,UC(0x00B4),\
+MT(MOD_LCTL,KC_ESC),KC_NO,KC_NO,   KC_NO,   KC_NO, KC_LCBR,                      KC_RCBR, KC_CIRC, KC_TILD,  KC_GRV,UC(0x00B4),KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSPO,   KC_NO,   KC_NO,UC(0x00E7), KC_NO, KC_LBRC,                      KC_RBRC, KC_PIPE, KC_ASTR, KC_PEQL, KC_PLUS, KC_RSPC,\
+      KC_LSPO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_LBRC,                      KC_RBRC, KC_BSLS, KC_QUES, KC_PEQL, KC_PLUS, KC_RSPC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, KC_LALT,   MO(3),     KC_SPC,   KC_NO, KC_TRNS \
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [_ADJUST] = LAYOUT( \
+  [_FMedia] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                        KC_NO, KC_MPRV, KC_MNXT, KC_MSTP,   KC_NO,   KC_NO,\
+        SHRUG,   KC_NO,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                        KC_NO, KC_MPRV, KC_MNXT, KC_MSTP,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_CAPS,   KC_NO,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY,   KC_NO,   KC_NO,\
+      KC_CAPS,   KC_NO,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                      KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY,   KC_NO,  KEYMAP,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                        KC_NO, KC_BRID, KC_BRIU,   KC_NO,   KC_NO, KC_PAUS,\
+        KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                        KC_NO, KC_BRID, KC_BRIU,   KC_NO,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                             RESET,   KC_NO, KC_TRNS,     KC_SPC,   KC_NO,   KC_NO \
                                       //`--------------------------'  `--------------------------'
@@ -169,49 +169,46 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         persistent_default_layer_set(1UL<<_QWERTY);
       }
       return false;
-    case LOWER:
+    case NumNav:
       if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        layer_on(_NumNav);
+        update_tri_layer_RGB(_NumNav, _Symbols, _FMedia);
       } else {
-        layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        layer_off(_NumNav);
+        update_tri_layer_RGB(_NumNav, _Symbols, _FMedia);
       }
       return false;
-    case RAISE:
+    case Symbols:
       if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        layer_on(_Symbols);
+        update_tri_layer_RGB(_NumNav, _Symbols, _FMedia);
       } else {
-        layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+        layer_off(_Symbols);
+        update_tri_layer_RGB(_NumNav, _Symbols, _FMedia);
       }
       return false;
-    case ADJUST:
-        if (record->event.pressed) {
-          layer_on(_ADJUST);
-        } else {
-          layer_off(_ADJUST);
-        }
-        return false;
-    case RGB_MOD:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          rgblight_mode(RGB_current_mode);
-          rgblight_step();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
+    case FMedia:
+      if (record->event.pressed) {
+        layer_on(_FMedia);
+      } else {
+        layer_off(_FMedia);
+      }
       return false;
-    case RGBRST:
-      #ifdef RGBLIGHT_ENABLE
-        if (record->event.pressed) {
-          eeconfig_update_rgblight_default();
-          rgblight_enable();
-          RGB_current_mode = rgblight_config.mode;
-        }
-      #endif
+    case SHRUG:
+      if (record->event.pressed) {
+        SEND_STRING("¯\\_(ツ)_/¯");
+      } else {
+        // when keycode is released
+      }
       break;
-  }
+    case KEYMAP:
+      if (record->event.pressed) {
+        SEND_STRING("https://github.com/antonioxdias/qmk_firmware/tree/master/keyboards/crkbd/keymaps/antonioxdias");
+      } else {
+        // when keycode is released
+      }
+      break;
+
+    }
   return true;
 }
