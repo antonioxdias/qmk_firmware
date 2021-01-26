98a1ce41833eb1d0053e45709f657f0a78604e18
António Dias <adias@mosano.eu>
2021-01-26 11:28:07 +0000

    Implement homerow mods

--- keyboards/crkbd/keymaps/antonioxdias/keymap.c
+++ keyboards/crkbd/keymaps/antonioxdias/keymap.c
@@ -17,6 +17,18 @@ extern uint8_t is_master;
 #define _Symbols 2
 #define _FMedia 3
 
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define SFT_S LSFT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define ALT_F LALT_T(KC_F)
+
+// Right-hand home row mods
+#define ALT_J LALT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define SFT_L RSFT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   NumNav,
@@ -38,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-MT(MOD_LCTL,KC_ESC),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_ENT,\
+CTL_T(KC_ESC),   CTL_A,   SFT_S,   GUI_D,   ALT_F,    KC_G,                         KC_H,   ALT_J,   GUI_K,   SFT_L,CTL_SCLN,  KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSPO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_MINS, KC_RSPC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -51,7 +63,7 @@ MT(MOD_LCTL,KC_ESC),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,                     
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_PSCR,   KC_NO,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_HOME, KC_PGUP, KC_PGDN,  KC_END,   KC_NO, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-MT(MOD_LCTL,KC_ESC),KC_NO, KC_4,    KC_5,    KC_6,    KC_0,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,   KC_NO,  KC_INS,\
+CTL_T(KC_ESC),   KC_NO,    KC_4,    KC_5,    KC_6,    KC_0,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,   KC_NO,  KC_INS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSPO,   KC_NO,    KC_1,    KC_2,    KC_3,    KC_0,                        KC_NO,   KC_NO, KC_PCMM, KC_PDOT,   KC_NO, KC_RSPC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -63,7 +75,7 @@ MT(MOD_LCTL,KC_ESC),KC_NO, KC_4,    KC_5,    KC_6,    KC_0,                     
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_PIPE, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_AMPR, KC_SLSH, KC_QUOT, KC_DQUO, KC_ASTR, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-MT(MOD_LCTL,KC_ESC),KC_NO,KC_NO, LITERAL,ARROWFUNC,KC_LCBR,                      KC_RCBR, KC_BSLS, KC_QUES, KC_PEQL, KC_PLUS, KC_ENT,\
+CTL_T(KC_ESC),   KC_NO,   KC_NO, LITERAL,ARROWFUNC,KC_LCBR,                      KC_RCBR, KC_BSLS, KC_QUES, KC_PEQL, KC_PLUS, KC_ENT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSPO,   KC_NO,   KC_NO,   KC_LT,   KC_GT, KC_LBRC,                      KC_RBRC, KC_CIRC, KC_TILD,  KC_GRV,   KC_NO, KC_RSPC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
