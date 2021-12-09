81fb216732ccce8321fd0e8ce0dbf4cff9e251af
António Dias <a.fortuna.dias@gmail.comm>
2021-12-09 18:05:36 +0000

    Adjust encoder resolution to match manufacturer specs

--- keyboards/x_pad/config.h
+++ keyboards/x_pad/config.h
@@ -19,5 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
+#define ENCODER_RESOLUTION 2
+
 #define ENCODERS_PAD_A { B1 }
 #define ENCODERS_PAD_B { B6 }
